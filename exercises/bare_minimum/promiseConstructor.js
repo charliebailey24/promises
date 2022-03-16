d182e50a24d49fb9da7b1bc7be764557aaf69ebb
Charlie <charliebailey24@gmail.com>
2022-03-15 20:46:15 -0700

    WIP on initial promises functions

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -9,8 +9,38 @@ var request = require('needle');
 var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
+
+// var pluckFirstLineFromFileAsync =
+
+
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    // do something asynchronous which eventually calls either:
+    //
+    //   resolve(someValue)        // fulfilled
+    // or
+    //   reject("failure reason")  // rejected
+    fs.readFile(filePath, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        var line = data.toString().split('\n')[0];
+        resolve(line);
+      }
+    });
+  }).catch(function(e) {
+    console.error(e.message);
+  });
+
+
+
+
+  // var promise = new Promise;
+  // fs.readFile(filePath, promise.then(data => {
+  //   var line = data.toString.split('\n')[0];
+  //   return line;
+  // }));
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
